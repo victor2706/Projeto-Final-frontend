c419012d01e4c11094be3997a9998dd704bb77c4
Victor2706 <vmanoel1727@gmail.com>
2022-05-30 21:48:22 -0300

    Autenticação usuario

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
@@ -13,4 +14,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 export const auth = getAuth()
--- src/pages/Novo/Novo.jsx
+++ src/pages/Novo/Novo.jsx
@@ -3,10 +3,40 @@ import MenuLat from "../../components/MenuLateral/Menulat"
 import Menunotopo from "../../components/Menunotopo/Menutopo"
 import FileUploadSharpIcon from '@mui/icons-material/FileUploadSharp';
 import { useState } from "react";
+import {addDoc, collection, doc,serverTimestamp,setDoc, Timestamp} from "firebase/firestore";
+import {auth ,db} from "../../firebase"
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Novo = ({inputs, title}) => {
 
   const [file, setFile] = useState("");
+  const [data, setData] = useState({})
+
+  const handleInput = (e) => {
+    const id = e.target.id;
+    const value = e.target.value;
+
+    setData({ ...data, [id]: value });
+  };
+
+  console.log(data)
+
+  const handleAdd = async (e) => {
+    e.preventDefault()
+    try{
+      const res = await createUserWithEmailAndPassword(
+        auth, 
+        data.email, 
+        data.password
+        );
+      await setDoc(doc(db, "usuarios", res.usuario.uid), {
+        ...data,
+      timestamp: serverTimestamp()
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  };
 
   console.log(file)
 
@@ -27,7 +57,7 @@ const Novo = ({inputs, title}) => {
             alt="" />
           </div>
           <div className="right">
-            <form>
+            <form onSubmit={handleAdd}>
             <div className="formInput">
                 <label htmlFor="file">
                   Imagem: <FileUploadSharpIcon className="icon" />
@@ -39,10 +69,14 @@ const Novo = ({inputs, title}) => {
               {inputs.map((input) =>(
                 <div className="formInput" key={input.id}>
                   <label>{input.label}</label>
-                  <input type={input.type} placeholder={input.placeholder} />
+                  <input
+                  id={input.id}
+                  type={input.type} 
+                  placeholder={input.placeholder} 
+                  onChange={handleInput}/>
                 </div>
               ))}
-              <button>Enviar</button>
+              <button type="submit">Enviar</button>
             </form>
           </div>
         </div>
